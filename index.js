b6f0882e9b3165c3d9841b4512dbcd6655131bdb
Anna Maria Lima <87787347+Annmochii@users.noreply.github.com>
2022-12-04 11:10:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ $(document).on('ready', function() {
         slidesToScroll: 1
     });
 });
+
+let team = ["analaura","annamaria","eloisa","raquel"];
+let selectedMember = 1;
+
+document.getElementById("carrosselarrowcontainer").addEventListener('click',()=>{
+    team.forEach(member => {
+        document.getElementById(member).style.display="none";
+    });
+    document.getElementById(team[selectedMember]).style.display="block";
+    selectedMember++
+    if (selectedMember>3){
+        selectedMember=0
+    }
+})
