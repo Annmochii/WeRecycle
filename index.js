a29227cbb8be3b93b188f7408163fa98a6f1c977
Raquel Rigoni <110852214+raqs-bot@users.noreply.github.com>
2022-12-06 16:05:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,22 +33,3 @@ btn[3].onclick = function(){
   this.classList.add("active");
 }
 
-$(document).on('ready', function() {
-  $(".regular").slick({
-      slidesToShow: 3,
-      slidesToScroll: 1
-  });
-});
-
-let team = ["analaura","annamaria","eloisa","raquel"];
-let selectedMember = 1;
-document.getElementById("carrosselarrowcontainer").addEventListener('click',()=>{
-team.forEach(member => {
-    document.getElementById(member).style.display="none";
-});
-document.getElementById(team[selectedMember]).style.display="block";
-selectedMember++
-if (selectedMember>3){
-    selectedMember=0
-}
-})
